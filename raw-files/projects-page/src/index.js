131b5f550136559f3530e8f77ae4390b864f12c2
Xanderxx <main@xanderxx.xyz>
2025-02-08 14:19:16 -0500

    attempt to make a projects page

---
+++ raw-files/projects-page/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ProjectsPage from "./projects-page";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<ProjectsPage />
+	</React.StrictMode>,
+);
